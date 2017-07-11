fb5f8a36bfc1d0be547c98c7a5ed10ebc4140f0e
Ilyua Chumak <old.flanigan@gmail.com>
2017-07-11 14:48:47 +0300

    production console interface

--- comparison.py
+++ comparison.py
@@ -4,9 +4,35 @@ import sys
 from pprint import pprint
 import time
 import xlrd
+import argparse
 
+def __parser():
+    parser = argparse.ArgumentParser(description='Comparison two xlsx tables',epilog="!!Support only excel!!")
+    parser.add_argument ('-p1','--path1',required=True,help='Path to first table for comparison')
+    parser.add_argument ('-p2','--path2',required=True,help='Path to second table for comparison')
+    return parser
 
-def comparison_xlrd(path_1,path_2):
+def __printProgressBar (iteration, total, prefix = 'Progress:', suffix = 'Complete',  decimals = 1, length = 50, fill = '█'):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r')
+    # Print New Line on Complete
+    if iteration == total:
+        print()
+
+def comparison(path_1,path_2):
 
     book_1 = xlrd.open_workbook(path_1)
     sheet_1 = book_1.sheet_by_index(0)
@@ -20,52 +46,30 @@ def comparison_xlrd(path_1,path_2):
     else:
         rows = sheet_1.nrows
         columns =  sheet_1.ncols
-
+        l = rows*columns
+        i = 0
+        __printProgressBar(i, l)
         for row in range(rows):
             for col in range(columns):
                 data_1 = sheet_1.cell_value(row,col)
                 data_2 = sheet_2.cell_value(row,col)
                 if data_1 != data_2:
                     Errors.append({'position': (row,col),
-                                    'data_1':data_1,
-                                    'data_2':data_2})
+                                    'in {}'.format(path_1):data_1,
+                                    'in {}'.format(path_2):data_2})
+                i+=1
+                __printProgressBar(i, l)
     return Errors
 
 if __name__=='__main__':
-    def comparison_pandas(path_1,path_2):
+    parser = __parser()
+    namespace = parser.parse_args()
+    pprint(comparison(namespace.path1,namespace.path2))
 
-        table_1 = pd.read_excel(path_1, na_values=['NA'])
-        table_2 = pd.read_excel(path_2, na_values=['NA'])
 
-        Errors = []
 
-        if len(table_1.keys())!= len(table_2.keys()):
-                sys.exit('Unsuitable files for comparison')
 
-        comp = table_1 == table_2
-        comp_m=comp.as_matrix()
-        print(np.where(comp == False))
 
-        for key in comp.keys():
-            for index in range(comp[key].size):
-                if not comp[key][index]:
-                    Errors.append((key,index))
-        return Errors
 
-    def time_decorator(function):
-        def timer(path_1,path_2):
-            start = time.time()
-            err = function(path_1,path_2)
-            stop = time.time()
-            sec = stop - start
-            print(sec)
-            return err
-        return timer
 
-    dec_comp_xlrd = time_decorator(comparison_xlrd)
-    dec_comp_pandas = time_decorator(comparison_pandas)
 
-    print('Без pandas:')
-    pprint(dec_comp_xlrd('./hostels.xlsx', './hostels1.xlsx'))
-    print('С pandas:')
-    pprint(dec_comp_pandas('./hostels.xlsx', './hostels1.xlsx'))
