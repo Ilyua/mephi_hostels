b9f7c551df1cbfe3dd3982139d5ded89c9aeff52
Ilyua Chumak <old.flanigan@gmail.com>
2017-07-11 14:25:12 +0300

    start console interface

--- comparison.py
+++ comparison.py
@@ -1,32 +1,9 @@
 import pandas as pd
+import numpy as np
 import sys
 from pprint import pprint
 import time
 import xlrd
-def comparison_pandas(path_1,path_2):
-
-    table_1 = pd.read_excel(path_1)
-    table_2 = pd.read_excel(path_2)
-
-    Errors = []
-    if table_1.keys().all() != table_2.keys().all():
-            sys.exit('Unsuitable files for comparison')
-    for key in table_1.keys():
-        if table_1[key].size != table_2[key].size:
-            sys.exit('Unsuitable files for comparison')
-
-    for key in table_1.keys():
-        for index in range(table_1[key].size):
-            data_1=table_1[key][index]
-            data_2=table_2[key][index]
-            if data_1 != data_2:
-                if ((pd.isnull(data_1)==True) and (pd.isnull(data_2)==True)):
-                    pass
-                else:
-                    Errors.append({'position': (key,index),
-                                   'data_1':data_1,
-                                   'data_2':data_2})
-    return Errors
 
 
 def comparison_xlrd(path_1,path_2):
@@ -54,25 +31,41 @@ def comparison_xlrd(path_1,path_2):
                                     'data_2':data_2})
     return Errors
 
+if __name__=='__main__':
+    def comparison_pandas(path_1,path_2):
 
-def time_decorator(function):
-    def timer(path_1,path_2):
-        start = time.time()
-        err = function(path_1,path_2)
-        stop = time.time()
-        sec = stop - start
-        print(sec)
-        return err
-    return timer
+        table_1 = pd.read_excel(path_1, na_values=['NA'])
+        table_2 = pd.read_excel(path_2, na_values=['NA'])
 
-dec_comp_xlrd = time_decorator(comparison_xlrd)
-dec_comp_pandas = time_decorator(comparison_pandas)
+        Errors = []
 
-print('Без pandas:')
-pprint(dec_comp_xlrd('./hostels.xlsx','./hostels1.xlsx'))
-print('С pandas:')
-pprint(dec_comp_pandas('./hostels.xlsx','./hostels1.xlsx'))
+        if len(table_1.keys())!= len(table_2.keys()):
+                sys.exit('Unsuitable files for comparison')
 
+        comp = table_1 == table_2
+        comp_m=comp.as_matrix()
+        print(np.where(comp == False))
 
+        for key in comp.keys():
+            for index in range(comp[key].size):
+                if not comp[key][index]:
+                    Errors.append((key,index))
+        return Errors
 
+    def time_decorator(function):
+        def timer(path_1,path_2):
+            start = time.time()
+            err = function(path_1,path_2)
+            stop = time.time()
+            sec = stop - start
+            print(sec)
+            return err
+        return timer
 
+    dec_comp_xlrd = time_decorator(comparison_xlrd)
+    dec_comp_pandas = time_decorator(comparison_pandas)
+
+    print('Без pandas:')
+    pprint(dec_comp_xlrd('./hostels.xlsx', './hostels1.xlsx'))
+    print('С pandas:')
+    pprint(dec_comp_pandas('./hostels.xlsx', './hostels1.xlsx'))
